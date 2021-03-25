791ab235fad8536b005834a77357fbb0e5b26058
Andrea <git@andreaemmanuele.it>
2021-03-25 17:34:23 +0100

    Add, edit, delete completed - check in progress

--- assets/js/createItem.js
+++ assets/js/createItem.js
@@ -1,20 +1,45 @@
-let app = new Vue({
+new Vue({
     el: "#root",
     data: {
         newTask: "",
-        tasks: [
-            {
-                title: "prova"
-             }
-        ]
+        onEdit: false,
+        taskEdited: "",
+        tasks: [],
+        tasksDone: [],
+        emptyTasks: "there aren't tasks to do, let's add one task"
     },
     methods: {
         add: function () {
-            let task = {
-                title: this.newTask
-            }
+            if (this.newTask !== "") {
+                if (this.onEdit === true) {
+                    this.tasks[this.taskEdited].title = this.newTask;
+                    this.newTask = "";
+                    this.onEdit = false;
+                }
+                else {
+                    let task = {
+                        title: this.newTask
+                    }
 
-            this.tasks.push(task);
-        }
+                    this.tasks.push(task);
+                    this.newTask = "";
+                    document.getElementById("task").focus();
+                }
+            }
+        },
+        check: function (task, i) {
+            this.tasksDone.push(task);
+            this.deleteTask(i);
+            console.log(this.tasksDone);
+        },
+        edit: function (task, i) {
+            this.newTask = task;
+            this.onEdit = true;
+            this.taskEdited = i;
+            document.getElementById("task").focus();
+        },
+        deleteTask: function (i) {
+            this.tasks.splice(i, 1);
+        },
     }
 })
